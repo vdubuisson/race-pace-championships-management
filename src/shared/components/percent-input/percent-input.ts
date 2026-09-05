13dcb1101f7d7e53222a5caa5128efef28c820aa
vdubuisson <val.dub03@gmail.com>
2026-09-05 13:58:11 +0200

    update deps

--- src/shared/components/percent-input/percent-input.ts
+++ src/shared/components/percent-input/percent-input.ts
@@ -1,4 +1,4 @@
-import { afterNextRender, Component, ElementRef, input, viewChild } from '@angular/core';
+import { Component, input } from '@angular/core';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { TuiError, TuiIcon } from '@taiga-ui/core';
 import { TuiInputSlider, TuiTooltip } from '@taiga-ui/kit';
@@ -15,15 +15,4 @@ export class PercentInput {
 
   readonly tooltip = input<string>();
   readonly size = input<'l' | 'm'>('m');
-
-  readonly input = viewChild<ElementRef<HTMLInputElement>>('input');
-
-  constructor() {
-    afterNextRender({
-      read: () => {
-        this.input()!.nativeElement.focus();
-        this.input()!.nativeElement.blur();
-      },
-    });
-  }
 }
